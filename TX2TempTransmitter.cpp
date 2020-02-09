3cfb7d162714fc422d22ce186ba8b98c6fe915b0
Philipp Paris <paris.philipp@gmail.com>
2020-02-09 13:06:15 +0100

    fixed TX2 Transmitter protocol

--- TX2TempTransmitter.cpp
+++ TX2TempTransmitter.cpp
@@ -6,64 +6,70 @@ TX2TempTransmitter::TX2TempTransmitter(uint8_t sensorID, uint8_t pin) {
 }
 
 void TX2TempTransmitter::transmit(float temp) {
-  int tens = 10 - (int(temp) / 10);
-  int ones = 15 - (int(temp) % 10);
-  int tenths = 15 - (int(temp * 10.0) % 10);
+  uint8_t nibble[11];
+  // add 50 degree offset
+  temp += 50;
 
-  int checksum = (tens & 1) + (tens>>1 & 1) + (tens>>2 & 1) + (tens>>3 & 1);
-  checksum += (ones & 1) + (ones>>1 & 1) + (ones>>2 & 1) + (ones>>3 & 1);
-  checksum += (tenths & 1) + (tenths>>1 & 1) + (tenths>>2 & 1) + (tenths>>3 & 1);
-  checksum &= 1;  // clear everything except the last bit (even or odd count);
-  checksum ^= 1;  // flip the bit: checksum = 1 if an even number of bits
-
-  int crc = (sensorID>>4)+(sensorID&0xF)+checksum+tens+ones+tenths+tens+ones-4;
-  crc &= 0xF;  // throw away everything except the lower four bits
+  // preamble
+  nibble[0] = TX2_PREAMBLE_1;
+  nibble[1] = TX2_PREAMBLE_2;
+  nibble[2] = TX2_TYPE_TEMP;
+  // temperature
+  nibble[5] = nibble[8] = int(temp) / 10;
+  nibble[6] = nibble[9] = int(temp) % 10;
+  nibble[7] = int(temp * 10.0) % 10;
+  // sensor-id and parity
+  nibble[3] = sensorID >> 4;
+  nibble[4] = sensorID & 0xE | calcParity(nibble);
+  //crc
+  nibble[10] = calcCRC(nibble);
   
-  // send data
-  for (int i = 0; i < 2; ++i) { // send the sequence twice
-    sendModelID();
-    sendSensorID();
-    if (checksum) I(); else O();
-    sendNibble(tens); sendNibble(ones); sendNibble(tenths);
-    sendNibble(tens); sendNibble(ones); sendNibble(crc);
-    delay(20);
+  // send the sequence twice
+  for (uint8_t i = 0; i < 2; ++i) { 
+    for (uint8_t j = 0; j < 11; j++) {
+      sendNibble(nibble[j]);
+    }
+    delay(50);
   }
 }
 
 void TX2TempTransmitter::I() {
+  digitalWrite(pin, HIGH); 
+  delayMicroseconds(TX2_SHORT_PULSE); 
+  digitalWrite(pin, LOW); 
+  delayMicroseconds(TX2_DELAY);
+}
+
+void TX2TempTransmitter::O() {
   digitalWrite(pin, HIGH); 
   delayMicroseconds(TX2_LONG_PULSE); 
   digitalWrite(pin, LOW); 
   delayMicroseconds(TX2_DELAY); 
 }
 
-void TX2TempTransmitter::O() {
-  digitalWrite(pin, HIGH); 
-  delayMicroseconds(TX2_SHORT_PULSE); 
-  digitalWrite(pin, LOW); 
-  delayMicroseconds(TX2_DELAY); 
-}
-
-void TX2TempTransmitter::sendNibble(int i) {
+void TX2TempTransmitter::sendNibble(uint8_t i) {
   if (i&8) I(); else O();
   if (i&4) I(); else O();
   if (i&2) I(); else O();
   if (i&1) I(); else O();
 }
 
-void TX2TempTransmitter::sendModelID() { 
-  I();I();I();I();O();I();O();I();I();I();I();I();
+uint8_t TX2TempTransmitter::calcCRC(uint8_t nibble[]) {
+  int crc = 0;
+  for (uint8_t i = 0; i < 10; i++) {
+    crc += nibble[i];
+  }
+  return crc & 0xF;
 }
 
-void TX2TempTransmitter::sendSensorID() {
-  // because it is easier to look at the sensor ID as it appears in the sequence
-  // with a checksum at the end, we use a value that has an extra (unused)
-  // bit at the end, where the checksum would go, e.g., ssss sssc
-  if (sensorID & 0x80) I(); else O();
-  if (sensorID & 0x40) I(); else O();
-  if (sensorID & 0x20) I(); else O();
-  if (sensorID & 0x10) I(); else O();
-  if (sensorID & 0x08) I(); else O();
-  if (sensorID & 0x04) I(); else O();
-  if (sensorID & 0x02) I(); else O();
+uint8_t TX2TempTransmitter::calcParity(uint8_t nibble[]) {
+  uint8_t parity = 0;
+  for (uint8_t i = 5; i < 8; i++) {
+    parity += nibble[i] & 1;
+    parity += nibble[i] >> 1 & 1;
+    parity += nibble[i] >> 2 & 1;
+    parity += nibble[i] >> 3 & 1;
+  }
+  return parity & 1;
 }
+
--- TX2TempTransmitter.h
+++ TX2TempTransmitter.h
@@ -3,10 +3,27 @@
 
 #include <ESP8266WiFi.h>
 
-#define TX2_LONG_PULSE (975)
-#define TX2_SHORT_PULSE (250)
+#define TX2_LONG_PULSE (1300)
+#define TX2_SHORT_PULSE (500)
 #define TX2_DELAY (1450)
 
+#define TX2_PREAMBLE_1 (0)
+#define TX2_PREAMBLE_2 (10)
+#define TX2_TYPE_TEMP (0)
+
+/*
+ * example transmission:
+ *  [00001010 | 0000 |  1100110   |    1   |0101|0010| 0110 |0101|0010|0111]
+ *  [preamble | type | sensor id  | parity |tens|ones|tenths|tens|ones| crc]
+ * 
+ * type: some transmitters can transmit temperature (0000) and humidity (1110)
+ * sensor-id: id of the sensor. is reset randomly if battery swapped
+ * parity: even parity of 'tens', 'ones' and 'tenths'
+ * crc: 4 lowest bits of arithmetic sum of all nibbles
+ * 
+ * temperature with offset: 52.6°C
+ * temperature: 2.6°C
+ */
 class TX2TempTransmitter {
   public:
     TX2TempTransmitter(uint8_t sensorID, uint8_t pin);
@@ -15,9 +32,9 @@ class TX2TempTransmitter {
   private:
     void I();   // sends one
     void O();   // sends zero
-    void sendModelID();
-    void sendSensorID();
-    void sendNibble(int i);
+    void sendNibble(uint8_t i);
+    uint8_t calcCRC(uint8_t nibble[]);
+    uint8_t calcParity(uint8_t nibble[]);
     uint8_t pin = 0;
     uint8_t sensorID = 0;
 };
